4a5ba7e1d3ef2ffb4d612bb3d15d25e84a3d499f
Vako <valeri.mekhashishvili97@gmail.com>
2024-05-17 21:39:20 +0400

    added cell

---
+++ Collab N1 N-27/Scenes/WeatherPage/Cell/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  Collab N1 N-27
+//
+//  Created by valeri mekhashishvili on 17.05.24.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
